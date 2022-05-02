e32ebde1dbcb0d005e0d6db8438d2fccae81ca28
milankj <milanjain005@gmail.com>
2022-05-02 23:05:05 +0530

    doubt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const User = require('../models/userModel')
-
+const {promisify} = require('util')
 const jwt = require('jsonwebtoken')
+const { default: isFloat } = require('validator/lib/isFloat')
+const { token } = require('morgan')
 
 const signToken = (id)=>{
     return jwt.sign(
@@ -38,19 +40,36 @@ exports.signup = async(req,res,next)=>{
           })
     }
 }
-
+exports.protect = async(req,res,next)=>{
+    
+    let token
+    //  1. GETTING TOKEN AND CHECK IF ITS THERE
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        token = req.headers.authorization.split(' ')[1] 
+    }
+    if(!token){
+        console.log('User not logged in ')
+        return    
+    }
+    //  2.VERIFICATION TOKEN
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);   
+    console.log(decoded)
+    //  3.CHECK IF USER STILL EXISTS
+    //  4.CHECK IF USER CHANGED PASSWORD AFTER  TOKEN WAS ISSUED
+    next()
+}
 exports.login = async (req,res)=>{
     try
     {
         
     const {email,password}= req.body
     //1)CHECK IF EMAIL AND PASSWORD EXIST
-    if(!email || !password){
-       return res.status(400).json({
-            status:"Failed",
-            message:"Provide proper Login"
-        })
-    }
+        if(!email || !password){
+        return res.status(400).json({
+                status:"Failed",
+                message:"Provide proper Login"
+            })
+        }
 
     //2)CHECK IF USER AND PASSWORD IS CORRECT
     const user = await User.findOne({email}).select('+password')
